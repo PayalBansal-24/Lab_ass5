d7ccc0103739514ed355fc9f0a4795dd6374e183
PayalBansal-24 <137813552+PayalBansal-24@users.noreply.github.com>
2023-12-06 18:59:34 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class lab_ass5_1
+{
+    static void Main()
+    {
+        int[] numbers = { 10, 20, 30, 40, 50 };
+
+        int indexToAccess = 7;
+
+        try
+        {
+            int value = numbers[indexToAccess];
+            Console.WriteLine($"Value at index {indexToAccess}: {value}");
+        }
+        catch (IndexOutOfRangeException ex)
+        {
+            Console.WriteLine("Index is out of range. Please access a valid index within the array bounds.");
+
+        }
+    }
+}
