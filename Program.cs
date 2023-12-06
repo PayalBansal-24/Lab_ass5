a5e7259ad95ed7d0f2f0f072a7d9ba06d9b89b5b
PayalBansal-24 <137813552+PayalBansal-24@users.noreply.github.com>
2023-12-06 19:09:44 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Program7
+{
+    static void Main()
+    {
+        bool validInput = false;
+
+        while (!validInput)
+        {
+            try
+            {
+                Console.Write("Enter the numerator: ");
+                int numerator = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter the denominator: ");
+                int denominator = int.Parse(Console.ReadLine());
+
+                if (denominator == 0)
+                {
+                    throw new DivideByZeroException("Cannot divide by zero. Please enter a non-zero denominator.");
+                }
+
+                int result = DivideNumbers(numerator, denominator);
+                Console.WriteLine($"Result of division: {result}");
+
+                validInput = true;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input. Please enter valid integers.");
+
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+        }
+    }
+
+    static int DivideNumbers(int numerator, int denominator)
+    {
+        return numerator / denominator;
+    }
+}
