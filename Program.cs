24e0d6d4023cb9f1df1a0d4b64cfc8f7f3b93573
PayalBansal-24 <137813552+PayalBansal-24@users.noreply.github.com>
2023-12-06 19:08:36 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class multipletryblock
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            try
+            {
+                int a = 25;
+                int b = 0;
+                int result = a / b;
+                Console.WriteLine("Result: " + result);
+            }
+            catch (DivideByZeroException i)
+            {
+                Console.WriteLine("Inner catch block: " + i.Message);
+                throw;
+            }
+        }
+        catch (Exception o)
+        {
+            Console.WriteLine("Outer catch block: " + o.Message);
+        }
+        Console.ReadLine();
+    }
+}
