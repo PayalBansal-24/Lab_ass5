7a79ab54e3c4d4fe50517e22c08e57a905c27ed7
PayalBansal-24 <137813552+PayalBansal-24@users.noreply.github.com>
2023-12-06 19:07:21 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace multipleException
+{
+    class multipleException
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                int[] a = new int[5];
+                Console.WriteLine(a[6]);
+
+                string s = Console.ReadLine();
+                int n = int.Parse(s);
+                throw new InvalidOperationException("Invalid operation 										perform");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
