00c7898eb694b651dedff3adaa2a19d93d1c9281
PayalBansal-24 <137813552+PayalBansal-24@users.noreply.github.com>
2023-12-06 19:10:53 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program8
+{
+    static void Main()
+    {
+        try
+        {
+            TryRethrowException();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Exception caught in the Main method:");
+            Console.WriteLine(ex.Message);
+
+        }
+    }
+
+    static void TryRethrowException()
+    {
+        try
+        {
+
+            PerformDivision(10, 0);
+        }
+        catch (Exception originalException)
+        {
+            Console.WriteLine("Exception caught in the TryRethrowException method:");
+            Console.WriteLine(originalException.Message);
+
+            throw;
+        }
+    }
+
+    static int PerformDivision(int numerator, int denominator)
+    {
+        if (denominator == 0)
+        {
+            throw new DivideByZeroException("Division by zero is not allowed.");
+        }
+
+        return numerator / denominator;
+    }
+}
