841e3a2103bc610eb7e3d6847de7153b07b1ce6f
PayalBansal-24 <137813552+PayalBansal-24@users.noreply.github.com>
2023-12-06 19:03:52 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Class1
+{
+    public Class1()
+    {
+    }
+}
+class Program3
+{
+    static void Main()
+    {
+        string userInput = "ABC";
+
+        try
+        {
+            int parsedInt = int.Parse(userInput);
+            Console.WriteLine($"Parsed integer: {parsedInt}");
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Invalid input format. Please enter a valid integer.");
+
+        }
+    }
+}
