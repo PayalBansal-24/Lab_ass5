5b8fb6b334ec8d2cc91721791315bf199f4fa6b9
PayalBansal-24 <137813552+PayalBansal-24@users.noreply.github.com>
2023-12-06 19:02:01 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class lab_ass5_2
+{
+    static void Main()
+    {
+        try
+        {
+            int numerator = 10;
+            int denominator = 0;
+
+            int result = PerformDivision(numerator, denominator);
+            Console.WriteLine($"Result of division: {result}");
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Divide by zero error occurred. Please provide a non-zero denominator.");
+
+        }
+    }
+
+    static int PerformDivision(int numerator, int denominator)
+    {
+        if (denominator == 0)
+        {
+            throw new DivideByZeroException();
+        }
+
+        return numerator / denominator;
+    }
+}
