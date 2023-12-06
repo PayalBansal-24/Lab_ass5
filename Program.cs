22b457c09229aad3340b65e4e89fcd58faae954b
PayalBansal-24 <137813552+PayalBansal-24@users.noreply.github.com>
2023-12-06 19:05:39 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+class CustomException : Exception
+{
+    public CustomException(string message) : base(message)
+    {
+
+    }
+}
+
+class lab_ass5_4
+{
+    static void Main()
+    {
+        try
+        {
+            int value = 100;
+
+            if (value == 100)
+            {
+
+                throw new CustomException("Custom Exception: Value is 100.");
+            }
+        }
+        catch (CustomException ex)
+        {
+            Console.WriteLine("Custom Exception caught:");
+            Console.WriteLine(ex.Message);
+
+        }
+    }
+}
